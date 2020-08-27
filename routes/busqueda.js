cce2e09fe02cc39b83033cffa52e9178749c2b84
jorgetinto <jpinogaray@gmail.com>
2020-08-27 18:10:52 -0400

    Fin seccion 11

---
+++ routes/busqueda.js
@@ -0,0 +1,16 @@
+/*
+    Ruta: /api/todo:busqueda
+*/
+
+const { Router } = require('express');
+const { validarJWT } = require('../helpers/validar-jwt');
+
+const { getTodo, getDocumentosColeccion } = require('../controllers/busqueda');
+
+const router = Router();
+
+router.get('/:busqueda', validarJWT, getTodo);
+router.get('/coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion);
+
+
+module.exports = router;
---
+++ routes/medicos.js
@@ -0,0 +1,40 @@
+/*
+    Hosptales
+    ruta: /api/medicos
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { validarJWT } = require('../helpers/validar-jwt');
+
+const {
+    getMedicos,
+    crearMedico,
+    actualizarMedico,
+    borrarMedico
+} = require('../controllers/medicos');
+
+const router = Router();
+
+router.get('/', getMedicos);
+
+router.post('/', 
+    [
+        validarJWT,
+        check('nombre', 'El nombre del Medico es necesario').not().isEmpty(),
+        check('hospital', 'El hospital id debe ser valido').isMongoId(),
+        validarCampos       
+    ],
+    crearMedico);
+
+router.put('/:id', 
+    [
+        
+    ],
+    actualizarMedico);
+
+router.delete('/:id', validarJWT, borrarMedico);
+
+module.exports = router;
